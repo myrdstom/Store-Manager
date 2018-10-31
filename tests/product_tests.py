5028c61ca394683166bcb208a4104c590370b16a
myrdstom <nserekopaul@gmail.com>
2018-10-31 22:04:58 +0300

    chore: Implementing tests for sales endpoint[#161204053]

--- tests/product_tests.py
+++ tests/product_tests.py
@@ -6,7 +6,7 @@ product_data = dict(product_name="Acer",
                     unit_price=19000000,
                     stock=100)
 
-sale_data = dict(product_id=1,
+sale_data = dict(product_name="Acer",
                  quantity=32)
 
 empty_product_data = {}
@@ -164,15 +164,25 @@ class FlaskTestCase(BaseTestCase):
 #     # """Sales tests"""
 #     #
 #     # """testing  GET all sales"""
-#     #
-#     # def test_get_all_items_sold(self):
-#     #     with self.app.test_client() as client:
-#     #         response = client.get("/api/v1/sales",
-#     #                               content_type="application/json",
-#     #                               data=json.dumps(sale_data))
-#     #         response_json = json.loads(response.data.decode())
-#     #         self.assertEqual(response.status_code, 200)
-#     #         self.assertIn("Acer", response_json[0]['product_name'])
+
+    def test_get_all_items_sold(self):
+        with self.app.test_client() as client:
+            response1 = client.post('/api/v1/products',
+                                    headers={'Content-Type': 'application/json',
+                                                                 'Authorization': 'Bearer ' +
+                                                                                  self.admin_login()[
+                                                                                      'access_token']},
+                                    data=json.dumps(product_data))
+            self.assertEqual(response1.status_code, 201)
+            response = client.post("/api/v1/sales",
+                                  headers={'Content-Type': 'application/json',
+                                           'Authorization': 'Bearer ' +
+                                                            self.login_user()[
+                                                                'access_token']},
+                                  data=json.dumps(sale_data))
+            response_json = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            # self.assertIn("Acer", response_json[0]['product_name'])
 #     #
 #     # """testing  GET a single sale"""
 #     #
