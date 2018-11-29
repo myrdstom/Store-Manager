153ab7e67acfc8ae759ec59f1acc8c6545f92536
myrdstom <nserekopaul@gmail.com>
2018-11-29 15:00:35 +0300

    chore: Bulking up gh-pages with javascript[#161108998]

---
+++ static/js/category.js
@@ -0,0 +1,77 @@
+document.getElementById('getText').addEventListener('click', getCategories);
+document.getElementById('deleteSingleText').addEventListener('click', deleteSingleCategory);
+function getCategories(){
+    fetch('https://store-manager-heroku.herokuapp.com/api/v1/categories',{
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem("access_token"),
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json'
+        }
+    })
+
+        .then((res) => res.json())
+            //console.log(res.json());
+        .then((data) => {
+            console.log(data);
+
+            if("msg" in data){
+                alert(data.msg)
+            }
+            if ("message" in data){
+                alert(data.message)
+            } else {
+                let output = '<h2> </h2>';
+                data.forEach(function (category) {
+                    output += `
+                <table>
+                <tbody>
+                <tr >
+                <td>${category.category_id}</td>
+                <td class = "category-list-group-item">${category.category_name}</td>
+
+                </tr>
+                </tbody>
+                </table>
+                `;
+                });
+
+
+                document.getElementById('output').innerHTML = output;
+            }
+
+        })
+
+    }
+function deleteSingleCategory(e){
+    e.preventDefault();
+    let categoryid = document.getElementById('myInput').value;
+    let category_id = parseInt(categoryid);
+    if (isNaN(category_id)){
+        alert("Please insert an ID")
+    }
+
+    fetch('https://store-manager-heroku.herokuapp.com/api/v1/categories/' + category_id,{
+        method: 'DELETE',
+        headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem("access_token"),
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json'
+        }
+    })
+
+         .then((res) => res.json())
+            //console.log(res.json());
+        .then((data) => {
+            console.log(data);
+
+            if("msg" in data){
+                alert(data.msg)
+            }
+            if ("message" in data){
+                alert(data.message)
+            }
+
+        })
+
+    }
